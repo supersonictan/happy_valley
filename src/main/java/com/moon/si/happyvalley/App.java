7bcef67fd9817de718ab967d111f0d83da0eb2df
tanzhen.tz <tanzhen.tz@alibaba-inc.com>
2017-02-07 14:28:07 +0800

    init commit

---
+++ src/main/java/com/moon/si/happyvalley/App.java
@@ -0,0 +1,10 @@
+package com.moon.si.happyvalley;
+
+/**
+ * Created by ZeYu
+ * Date: 2017/2/7.
+ * Time: 11:50.
+ * DESC: 没有用的类
+ */
+public class App {
+}
---
+++ src/main/java/com/moon/si/happyvalley/dao/base/BaseMapper.java
@@ -0,0 +1,26 @@
+package com.moon.si.happyvalley.dao.base;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by ZeYu
+ * Date: 2017/2/7.
+ * Time: 14:20.
+ * DESC: mapper.java 实现的接口，定义了增删改查
+ */
+public interface BaseMapper<T extends Serializable> {
+
+    /**增加**/
+    public Integer insert(T entity);
+
+    /**根据id更新数据**/
+    public Integer update(T entity);
+
+    /**批量删除**/
+    public Integer deleteByIds(List<Integer> ids);
+
+    /**批量查询**/
+    public List<T> findList(T param);
+
+}
